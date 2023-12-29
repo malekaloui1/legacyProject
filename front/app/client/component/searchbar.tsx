a121b487f34a46caa011d58bf4d435e4a6a2bff7
marwaZbidi <zbidimarwa0@gmail.com>
2023-12-29 10:02:54 +0100

    navbar + footer

--- front/app/client/component/searchbar.tsx
+++ front/app/client/component/searchbar.tsx
@@ -1,8 +1,9 @@
 "use client"
 import React,{useState} from 'react'
 import axios from "axios"
+import ProductProps from '../page' 
 
-const Searchbar = ({products}) => {
+const Searchbar = () => {
 
     const [searchData, setSearchData] = useState ([])
     const [search, setSearch] = useState("");
@@ -14,13 +15,18 @@ const Searchbar = ({products}) => {
 
   return (
     <div className="SearchBar">
+      <div className="flex items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 pt-0.5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
       <input
-      onChange={(e) => {
+       onChange={(e) => {
         setSearch(e.target.value);
       }}
-      className="ml-2 outline-none bg-transparent font-" type="text" name="search" id="search" placeholder="Search..."
-      />
-      <button onClick={()=>{handleSearch(products.name)}}>Search</button>
+       className="ml-2 outline-none bg-transparent font-" type="text" name="search" id="search" placeholder="Search..." />
+    </div>
+      
+      {/* <button onClick={()=>{handleSearch()}}>Search</button> */}
     </div>
   )
 }
--- front/app/client/page.tsx
+++ front/app/client/page.tsx
@@ -1,32 +1,49 @@
 import React from 'react'
-import Navbar from './component/navbar';
+import Navbar from './navbar/page'
+import Footer from'./component/footer'
+import Image from 'next/image'
+import axios from 'axios'
 
-interface Product{
-    id: number;
-    name: string;
-    price: number;
-    description: string;
+interface ProductProps{
+    id: number,
+    name: string,
+    price: number,
+    description: string,
     images: string
 }
 
-const ClientHome = async() => {
+const ClientHome = async(props:ProductProps) => {
 
     const res= await fetch("http://localhost:3000/client/getAllProduct")
-    const products: Product[] = await res.json()
+    const products: ProductProps[] = await res.json()
   
 
   return (
     <div className='flex flex-col min-h-screen'>
-    <Navbar products={products}/>
-    <main className='flex grow'>
-    {products.map(product=>
-        <div key={product.id}>
-            
-          <div>{product.name}</div>
-          <div>{product.price}</div>
-        </div>
-        )}
-        </main>
+    <Navbar/>
+    <div className=''>
+{/* <Image
+src="https://i.pinimg.com/564x/0b/bd/0f/0bbd0f185a0f564d80d17ea11147fe36.jpg"
+alt=''
+/> */}
+</div>
+<main>
+<div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+{products.map(props=>
+        <div key={props.id}>
+	{/* <Image fill={true} src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"/> */}
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-3xl font-semibold tracki">{props.name}</h2>
+			<p className="dark:text-gray-100">{props.price}</p>
+      </div>      
+		</div>
+   <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracki rounded-md dark:bg-violet-400 dark:text-gray-900">Read more</button>
+	</div>
+   )}
+</div>
+</main>
+        <Footer/>
         </div>
        
   )
